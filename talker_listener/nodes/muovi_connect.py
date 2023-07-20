81b2cca3c4fc750dc50bd4428b189dd54bb8a503
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2023-07-20 14:14:06 -0500

    Clean code for muovi_connect, implement a new streamer class to replace muovi_connect

--- talker_listener/nodes/muovi_connect.py
+++ talker_listener/nodes/muovi_connect.py
@@ -2,25 +2,11 @@ import socket
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
-TCPPort = 54320
-NumCycles = 20  # Number of data recordings
-Mode = 0  # 0=64Ch Monop, 1=64Ch with remove average, 2=64Ch ImpCk, 3=64Ch Test
-ConvFact = 0.000286  # Conversion factor for the bioelectrical signals to get the values in mV
-
-if Mode > 7:
-    print("Error, set ModeX values between 0 and 7")
-
-def dec2bin(n):
-    return bin(n).replace("0b", "")
-
-# Create the command to send to Muovi+ probe
-Command = 0
-Command = Mode * 2 + 9
-NumChan = 70
-sampFreq = 500
-bin(Command).replace("0b", "")  # Convert command to binary
+NUMCYCLES = 20  # Number of data recordings
+CONVFACT = 0.000286  # Conversion factor for the bioelectrical signals to get the values in mV
+NUMCHAN = 70
+SAMPFREQ = 512
 
 # Open the TCP socket as server
 t = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,43 +14,40 @@ t.bind(('0.0.0.0', 54321))
 print('Waiting for connection...')
 t.listen(1)
 conn, addr = t.accept()
-conn.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 500000)  # Increase the input buffer size
 print('Connected to the Muovi+ probe')
-conn.send(struct.pack('B', Command))  # Send the command to Muovi+ probe
-blockData = 2*NumChan*sampFreq  # Define the length of the data block to be recorded
+conn.send(struct.pack('B', 9))  # Send the command to Muovi+ probe
+blockData = 2*NUMCHAN*SAMPFREQ  # Define the length of the data block to be recorded
 
-for i in range(NumCycles):
-    # Receive data until the expected size of data is reached
+# Data processing/plotting
+for i in range(NUMCYCLES):
     data = b''
     while len(data) < blockData:
-        data += conn.recv(blockData - len(data))
-    data = np.frombuffer(data, dtype=np.int16).reshape((NumChan, sampFreq))  # Read one second of data into signed integer
+        data += conn.recv(blockData - len(data))  # Receive data until the expected size of data is reached
+    data = np.frombuffer(data, dtype=np.int16).reshape((NUMCHAN, SAMPFREQ))  # Read one second of data into signed integer
 
-    # Plot the EMG signals
+    # Plot the EMG signals (the first 64 channels)
     plt.subplot(2, 1, 1)
     plt.cla()
     plt.title('EMG signals')
-    for j in range(NumChan-6):
-        plt.plot(data[j, :]*ConvFact + (j-1))
-    plt.xlim([0, sampFreq])
-    plt.ylim([-1, ((NumChan-6))])
+    for j in range(NUMCHAN-6):
+        plt.plot(data[j, :]*CONVFACT + (j-1))
+    plt.xlim([0, SAMPFREQ])
+    plt.ylim([-1, ((NUMCHAN-6))])
 
-    # Plot the IMU
+    # Plot the IMU (the 3 channels from channel 64 to 67)
     plt.subplot(2, 1, 2)
     plt.cla()
     plt.title('IMU signals')
-    for j in range(NumChan-5, NumChan-2):
+    for j in range(NUMCHAN-5, NUMCHAN-2):
         plt.plot(data[j, :])
-    plt.xlim([0, sampFreq])
+    plt.xlim([0, SAMPFREQ])
     plt.ylim([-33000, 33000])
 
     # Animation
     plt.draw()
-    plt.pause(0.001)
+    plt.pause(0.0001)
 
 # Close the TCP socket
-conn.send(struct.pack('B', Command-1))  # Stop the data transfer
-time.sleep(0.5)
 conn.close()
 t.close()
 print("Disconnected from the Muovi+ probe")
