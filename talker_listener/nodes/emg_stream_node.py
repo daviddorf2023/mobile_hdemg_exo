5389687da6803ba634385834c05b7cbf826a61f7
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2023-08-02 11:35:52 -0500

    Create class structure for emg_stream_node and incorporate latency_analyzer into it. Add argument to launch file that can be toggled to enable it in emg_stream_node.

--- talker_listener/nodes/emg_stream_node.py
+++ talker_listener/nodes/emg_stream_node.py
@@ -8,52 +8,60 @@ from talker_listener.processors.emg_process_rms import EMGProcessorRMS
 from talker_listener.streamer.emg_file_streamer import EMGFileStreamer
 from talker_listener.streamer.emg_qc_streamer import EMGQCStreamer
 from talker_listener.streamer.emg_muovi_streamer import EMGMUOVIStreamer
+import RPi.GPIO as GPIO 
 
 # Launch file arguments
 EMG_DEVICE = rospy.get_param("/device")
 EMG_PROCESS_METHOD = rospy.get_param("/method")
+LATENCY_ANALYZER_MODE = rospy.get_param("/latency_analyzer")
 MUSCLE_COUNT = rospy.get_param("/muscle_count")
+PWM_OUTPUT_PIN = rospy.get_param("/pwm_output_pin")
 
-def topic_publish_reading(publisher: rospy.topics.Publisher, reading: "list[int]"):
-    stamped_sample = hdemg()
-    stamped_sample.header.stamp = rospy.get_rostime()
-    sample = Float64MultiArray()
-    sample.data = reading
-    sample.layout.dim = [
-        MultiArrayDimension("rows", 1, len(reading)),
-        MultiArrayDimension("columns", 1, 1)
-    ]
-    stamped_sample.data = sample
-    publisher.publish(stamped_sample)
+class EMGStreamNode:
+    def __init__(self):
+        self.p = GPIO.PWM(PWM_OUTPUT_PIN, 50) # 50 Hz
+        self.streamer = None  # Stream EMG data from the selected device
+        if EMG_DEVICE == 'qc':
+            self.streamer = EMGQCStreamer(MUSCLE_COUNT)
+        elif EMG_DEVICE == 'muovi':
+            self.streamer = EMGMUOVIStreamer(MUSCLE_COUNT)
+        elif EMG_DEVICE == 'file':
+            self.path = rospy.get_param("/file_dir")
+            self.path += "/src/talker_listener/raw_emg_34.csv"
+            self.streamer = EMGFileStreamer(MUSCLE_COUNT, 512, self.path)
+        self.streamer.initialize()
+        self.processor = None  # Process EMG data using the selected method
+        if EMG_PROCESS_METHOD == 'rms':
+            self.processor = EMGProcessorRMS()
+        elif EMG_PROCESS_METHOD == 'cst':
+            self.processor = EMGProcessorCST()  # TODO: Fix scikit-learn issues
+    
+    def topic_publish_reading(publisher: rospy.topics.Publisher, reading: "list[int]"):
+        stamped_sample = hdemg()
+        stamped_sample.header.stamp = rospy.get_rostime()
+        sample = Float64MultiArray()
+        sample.data = reading
+        sample.layout.dim = [
+            MultiArrayDimension("rows", 1, len(reading)),
+            MultiArrayDimension("columns", 1, 1)
+        ]
+        stamped_sample.data = sample
+        publisher.publish(stamped_sample)
 
+    def pwm_setup(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(PWM_OUTPUT_PIN, GPIO.OUT, initial=GPIO.HIGH)
+        self.p.start(50) # 50% duty cycle
+    
+    def pwm_cleanup(self):
+        self.p.stop()
+        GPIO.cleanup()
 
-if __name__ == '__main__':
-    rospy.init_node('emg_stream_node')
-    raw_pub = rospy.Publisher('hdEMG_stream', hdemg, queue_size=1)
-    processed_pub = rospy.Publisher('hdEMG_stream_processed', hdemg, queue_size=1)
-
-    streamer = None
-    if EMG_DEVICE == 'qc':
-        streamer = EMGQCStreamer(MUSCLE_COUNT)
-    elif EMG_DEVICE == 'muovi':
-        streamer = EMGMUOVIStreamer(MUSCLE_COUNT)
-    elif EMG_DEVICE == 'file':
-        path = rospy.get_param("/file_dir")
-        path += "/src/talker_listener/raw_emg_34.csv"
-        streamer = EMGFileStreamer(MUSCLE_COUNT, 512, path)
-    streamer.initialize()
-
-    processor = None
-    if EMG_PROCESS_METHOD == 'rms':
-        processor = EMGProcessorRMS()
-    elif EMG_PROCESS_METHOD == 'cst':
-        processor = EMGProcessorCST()  # TODO: Fix scikit-learn issues
-
-    r = rospy.Rate(streamer.sample_frequency)  # Match the streamer's publishing rate
-    while not rospy.is_shutdown():
-        emg_reading = streamer.stream_data()
-        topic_publish_reading(raw_pub, emg_reading)
-
+    def run_emg(self):
+        emg_reading = self.streamer.stream_data()
+        self.topic_publish_reading(raw_pub, emg_reading)
+        if LATENCY_ANALYZER_MODE:
+            self.pwm_setup()
         if EMG_DEVICE == 'qc': 
             # First MUSCLE_COUNT * 32 channels are for IN1..IN8, two INs per muscle
             offset = 128
@@ -62,16 +70,25 @@ if __name__ == '__main__':
             raw_muscle_reading = []
             for i in range(MUSCLE_COUNT):
                 raw_muscle_reading.append(hdemg_reading[i * 64:(i + 1) * 64])
-        if EMG_DEVICE == 'muovi': 
+        elif EMG_DEVICE == 'muovi': 
             # Each Muovi+ probe has 70 channels
             raw_muscle_reading = []
             # Keep only first 64 channels, last 6 are IMU data
             hdemg_reading = emg_reading[:64]
             for i in range(MUSCLE_COUNT):
                 raw_muscle_reading.append(emg_reading)
-
-        processor.process_reading(raw_muscle_reading)
-        processor.publish_reading(processed_pub)
+        self.processor.process_reading(raw_muscle_reading)
+        self.processor.publish_reading(processed_pub)
         r.sleep()
 
+if __name__ == '__main__':
+    rospy.init_node('emg_stream_node')
+    raw_pub = rospy.Publisher('hdEMG_stream', hdemg, queue_size=1)
+    processed_pub = rospy.Publisher('hdEMG_stream_processed', hdemg, queue_size=1)
+    streamer = EMGStreamNode()
+    r = rospy.Rate(streamer.sample_frequency)  # Match the streamer's publishing rate
+    while not rospy.is_shutdown():
+        streamer.run_emg()
+    if LATENCY_ANALYZER_MODE:
+        streamer.pwm_cleanup()
     streamer.close()
