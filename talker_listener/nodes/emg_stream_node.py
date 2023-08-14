6c8030ff0e39aad52678d156efb5cd2abfacba6a
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2023-08-13 21:53:20 -0500

    Add latency analyzer timestamping to various steps in emg_stream_node. Reduce PWM output to 1Hz and make sampling frequency a ROS parameter. Add docstrings and comments for future use. Eliminate auxiliary Quattrocento offset when in latency analyzer mode since it uses those ports.

--- talker_listener/nodes/emg_stream_node.py
+++ talker_listener/nodes/emg_stream_node.py
@@ -8,7 +8,8 @@ from talker_listener.processors.emg_process_cst import EMGProcessorCST
 from talker_listener.streamer.emg_file_streamer import EMGFileStreamer
 from talker_listener.streamer.emg_qc_streamer import EMGQCStreamer
 from talker_listener.streamer.emg_muovi_streamer import EMGMUOVIStreamer
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
+import time
 
 # Launch file arguments
 EMG_DEVICE = rospy.get_param("/device")
@@ -16,35 +17,68 @@ EMG_PROCESS_METHOD = rospy.get_param("/method")
 LATENCY_ANALYZER_MODE = rospy.get_param("/latency_analyzer")
 MUSCLE_COUNT = rospy.get_param("/muscle_count")
 PWM_OUTPUT_PIN = rospy.get_param("/pwm_output_pin")
+SAMPLING_FREQUENCY = rospy.get_param("/sampling_frequency")
 
 class EMGStreamNode:
+    """
+    A class for streaming and processing EMG data.
+
+    Attributes:
+        streamer: An object that streams EMG data from the selected device.
+        processor: An object that processes EMG data using the selected method.
+        raw_pub: A ROS publisher for publishing raw EMG data.
+        processed_pub: A ROS publisher for publishing processed EMG data.
+        r: A ROS rate object for controlling the publishing rate.
+    """
     def __init__(self):
+        """
+        Initializes the EMGStreamNode object.
+
+        Sets up the ROS publishers and initializes the EMG streamer and processor.
+        """
         self.streamer = None  # Stream EMG data from the selected device
         self.raw_pub = rospy.Publisher('hdEMG_stream', hdemg, queue_size=1)
         self.processed_pub = rospy.Publisher('hdEMG_stream_processed', hdemg, queue_size=1)
+
+        # Initialize the PWM output pin
         if LATENCY_ANALYZER_MODE:
+            self.start_time = time.time()
             GPIO.setmode(GPIO.BOARD)
             GPIO.setup(PWM_OUTPUT_PIN, GPIO.OUT, initial=GPIO.HIGH)
-            self.p = GPIO.PWM(PWM_OUTPUT_PIN, 50) # 50 Hz
+            self.p = GPIO.PWM(PWM_OUTPUT_PIN, 1) # 1 Hz
             self.p.start(50) # 50% duty cycle
+            self.pwm_time = (time.time() - self.start_time)
+            print("PWM started" + str(self.pwm_start_time))
+        
+        # Initialize the EMG streamer
         if EMG_DEVICE == 'qc':
             self.streamer = EMGQCStreamer(MUSCLE_COUNT)
         elif EMG_DEVICE == 'muovi':
             self.streamer = EMGMUOVIStreamer(MUSCLE_COUNT)
         elif EMG_DEVICE == 'file':
-            self.path = rospy.get_param("/file_dir")
-            self.path += "/src/talker_listener/raw_emg_34.csv"
-            self.streamer = EMGFileStreamer(MUSCLE_COUNT, 512, self.path)
+            self.path = rospy.get_param("/file_dir") + "/src/talker_listener/raw_emg_34.csv"
+            self.streamer = EMGFileStreamer(MUSCLE_COUNT, SAMPLING_FREQUENCY, self.path)
         rospy.init_node('emg_stream_node')
         self.r = rospy.Rate(self.streamer.sample_frequency)  # Match the streamer's publishing rate
         self.streamer.initialize()
+        if LATENCY_ANALYZER_MODE:
+            self.streamer_time = (time.time() - self.start_time)
+
+        # Initialize the EMG processor
         self.processor = None  # Process EMG data using the selected method
         if EMG_PROCESS_METHOD == 'rms':
             self.processor = EMGProcessorRMS()
         elif EMG_PROCESS_METHOD == 'cst':
             self.processor = EMGProcessorCST()
-    
+
     def topic_publish_reading(self, publisher: rospy.topics.Publisher, reading: "list[int]"):
+        """
+        Publishes an EMG reading to a ROS topic.
+
+        Args:
+            publisher: A ROS publisher object.
+            reading: A list of integers representing an EMG reading.
+        """
         stamped_sample = hdemg()
         stamped_sample.header.stamp = rospy.get_rostime()
         sample = Float64MultiArray()
@@ -61,13 +95,21 @@ class EMGStreamNode:
         GPIO.cleanup()
 
     def run_emg(self):
+        """
+        Runs the EMG streamer and processor.
+
+        Reads EMG data from the streamer, processes it using the selected method, and publishes the raw and processed data
+        to ROS topics.
+        """
         emg_reading = self.streamer.stream_data()
         self.topic_publish_reading(self.raw_pub, emg_reading)
-        if EMG_DEVICE == 'qc': 
-            # First MUSCLE_COUNT * 32 channels are for IN1..IN8, two INs per muscle
-            offset = 128
-            # Each MULTIPLE IN has 64 channels
-            hdemg_reading = emg_reading[offset:offset + MUSCLE_COUNT * 64]
+        if EMG_DEVICE == 'qc':
+            # The first 128 channels are from the auxiliary ports on the back of the Quattrocento
+            if LATENCY_ANALYZER_MODE:
+                offset = 0
+            else:
+                offset = 128
+            hdemg_reading = emg_reading[offset:offset + MUSCLE_COUNT * 64]  # Each MULTIPLE IN has 64 channels
             raw_muscle_reading = []
             for i in range(MUSCLE_COUNT):
                 raw_muscle_reading.append(hdemg_reading[i * 64:(i + 1) * 64])
@@ -82,7 +124,7 @@ class EMGStreamNode:
         self.processor.process_reading(raw_muscle_reading)
         self.processor.publish_reading(self.processed_pub)
         self.r.sleep()
-    
+
     def close(self):
         self.streamer.close()
 
