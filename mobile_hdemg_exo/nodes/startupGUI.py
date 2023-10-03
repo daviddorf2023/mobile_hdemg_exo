beba3ee108ad7644101ec00108c0b742b1c8dd1a
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2023-10-03 15:29:31 -0500

    Updated startup GUI options

--- mobile_hdemg_exo/nodes/startupGUI.py
+++ mobile_hdemg_exo/nodes/startupGUI.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import rospy
 
+
 class GUIApp:
     def __init__(self, root):
         self.root = root
@@ -25,54 +26,59 @@ class GUIApp:
     def create_widgets(self):
         device_label = ttk.Label(self.root, text="Device:")
         device_label.grid(row=0, column=0, padx=10, pady=5, sticky="w")
-
-        device_choices = ["Quattrocento", "MuoviPro", "Simulation"]
-        self.device_dropdown = ttk.Combobox(self.root, values=device_choices, textvariable=self.device_var)
+        device_choices = ["MuoviPro", "Quattrocento", "Simulation"]
+        self.device_dropdown = ttk.Combobox(
+            self.root, values=device_choices, textvariable=self.device_var)
         self.device_dropdown.grid(row=0, column=1, padx=10, pady=5, sticky="w")
 
         method_label = ttk.Label(self.root, text="Method:")
         method_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")
-
-        method_choices = ["RMS", "CST"]
-        self.method_dropdown = ttk.Combobox(self.root, values=method_choices, textvariable=self.method_var)
+        method_choices = ["RMS", "CST", "Raw"]
+        self.method_dropdown = ttk.Combobox(
+            self.root, values=method_choices, textvariable=self.method_var)
         self.method_dropdown.grid(row=1, column=1, padx=10, pady=5, sticky="w")
 
-        analyzer_label = ttk.Label(self.root, text="Analyzer:")
+        analyzer_label = ttk.Label(self.root, text="Latency analyzer:")
         analyzer_label.grid(row=2, column=0, padx=10, pady=5, sticky="w")
-
-        analyzer_choices = [True, False]
-        self.analyzer_dropdown = ttk.Combobox(self.root, values=analyzer_choices, textvariable=self.analyzer_var)
-        self.analyzer_dropdown.grid(row=2, column=1, padx=10, pady=5, sticky="w")
+        analyzer_choices = ["Off", "On"]
+        self.analyzer_dropdown = ttk.Combobox(
+            self.root, values=analyzer_choices, textvariable=self.analyzer_var)
+        self.analyzer_dropdown.grid(
+            row=2, column=1, padx=10, pady=5, sticky="w")
 
         side_label = ttk.Label(self.root, text="Side:")
         side_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")
-
         side_choices = ["Left", "Right"]
-        self.side_dropdown = ttk.Combobox(self.root, values=side_choices, textvariable=self.side_var)
+        self.side_dropdown = ttk.Combobox(
+            self.root, values=side_choices, textvariable=self.side_var)
         self.side_dropdown.grid(row=3, column=1, padx=10, pady=5, sticky="w")
 
         muscles_label = ttk.Label(self.root, text="Muscles:")
         muscles_label.grid(row=4, column=0, padx=10, pady=5, sticky="w")
-
         muscles_choices = ["1", "2", "3", "4"]
-        self.muscles_dropdown = ttk.Combobox(self.root, values=muscles_choices, textvariable=self.muscles_var)
-        self.muscles_dropdown.grid(row=4, column=1, padx=10, pady=5, sticky="w")
+        self.muscles_dropdown = ttk.Combobox(
+            self.root, values=muscles_choices, textvariable=self.muscles_var)
+        self.muscles_dropdown.grid(
+            row=4, column=1, padx=10, pady=5, sticky="w")
 
         trials_label = ttk.Label(self.root, text="Number of Trials [1 to 10]:")
         trials_label.grid(row=5, column=0, padx=10, pady=5, sticky="w")
-
         self.trials_var = tk.StringVar()
         self.trials_var.set("1")  # Default value
         trials_entry = ttk.Entry(self.root, textvariable=self.trials_var)
         trials_entry.grid(row=5, column=1, padx=10, pady=5, sticky="w")
 
-        start_button = ttk.Button(self.root, text="Start", command=self.start_process)
+        start_button = ttk.Button(
+            self.root, text="Start", command=self.start_process)
         start_button.grid(row=6, columnspan=2, pady=10)
 
     def start_process(self):
         selected_device = self.device_var.get()
         selected_method = self.method_var.get()
-        selected_analyzer = bool(self.analyzer_var.get())
+        if self.analyzer_var.get() == "On":
+            selected_analyzer = True
+        else:
+            selected_analyzer = False
         selected_side = self.side_var.get()
         selected_muscles = int(self.muscles_var.get())
         selected_trials = int(self.trials_var.get())
@@ -96,6 +102,7 @@ class GUIApp:
 
         # Set flag to completed
         rospy.set_param("/gui_completed", True)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
