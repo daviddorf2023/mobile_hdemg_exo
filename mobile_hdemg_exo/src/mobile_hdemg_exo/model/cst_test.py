73ffb93cf08647dd5b890594aee0ba31ec9a3e6d
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2023-10-12 11:07:50 -0500

    Remove CST test script, not necessary anymore

--- mobile_hdemg_exo/src/mobile_hdemg_exo/model/cst_test.py
+++
@@ -1,7 +0,0 @@
-import rospy
-from qc_predict import MUdecomposer
-
-path = rospy.get_param("/file_dir")
-model_file = path + "/src/mobile_hdemg_exo/" + \
-    "best_model_cnn-allrun5_c8b_mix4-SG0-ST20-WS40-MU[0, 1, 2, 3]_1644222946_f.lite"
-model = MUdecomposer(model_file)
