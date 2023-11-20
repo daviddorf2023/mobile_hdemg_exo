21c82fa733277d4c09453a419a820f2b688742fe
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2023-11-19 23:04:45 -0600

    Updates to GUI contents, size, and proper shutdown

--- mobile_hdemg_exo/nodes/startup_gui_node.py
+++ mobile_hdemg_exo/nodes/startup_gui_node.py
@@ -13,8 +13,9 @@ class GUIApp:
         self.exo_var = tk.StringVar()
         self.side_var = tk.StringVar()
         self.muscles_var = tk.StringVar()
-        self.root.geometry("500x320")
+        self.root.geometry("520x280")
         self.create_widgets()
+        rospy.on_shutdown(self.shutdown)
 
     def create_widgets(self):
         device_label = ttk.Label(self.root, text="Device:")
@@ -39,12 +40,13 @@ class GUIApp:
         self.analyzer_dropdown.grid(
             row=2, column=1, padx=10, pady=5, sticky="w")\
 
-        exo_label = ttk.Label(self.root, text="Exo Connection:")
-        exo_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")
-        exo_choices = ["True", "False"]
-        self.exo_dropdown = ttk.Combobox(
-            self.root, values=exo_choices, textvariable=self.exo_var)
-        self.exo_dropdown.grid(row=3, column=1, padx=10, pady=5, sticky="w")
+        muscles_label = ttk.Label(self.root, text="Muscles:")
+        muscles_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+        muscles_choices = ["1", "2", "3", "4"]
+        self.muscles_dropdown = ttk.Combobox(
+            self.root, values=muscles_choices, textvariable=self.muscles_var)
+        self.muscles_dropdown.grid(
+            row=3, column=1, padx=10, pady=5, sticky="w")
 
         side_label = ttk.Label(self.root, text="Side:")
         side_label.grid(row=4, column=0, padx=10, pady=5, sticky="w")
@@ -53,31 +55,23 @@ class GUIApp:
             self.root, values=side_choices, textvariable=self.side_var)
         self.side_dropdown.grid(row=4, column=1, padx=10, pady=5, sticky="w")
 
-        muscles_label = ttk.Label(self.root, text="Muscles:")
-        muscles_label.grid(row=5, column=0, padx=10, pady=5, sticky="w")
-        muscles_choices = ["1", "2", "3", "4"]
-        self.muscles_dropdown = ttk.Combobox(
-            self.root, values=muscles_choices, textvariable=self.muscles_var)
-        self.muscles_dropdown.grid(
-            row=5, column=1, padx=10, pady=5, sticky="w")
-
         remove_channels_label = ttk.Label(
             self.root, text="Channels to Remove (comma-separated):")
         remove_channels_label.grid(
-            row=6, column=0, padx=10, pady=5, sticky="w")
+            row=5, column=0, padx=10, pady=5, sticky="w")
         self.remove_channels_var = tk.StringVar()
         remove_channels_entry = ttk.Entry(
             self.root, textvariable=self.remove_channels_var)
         remove_channels_entry.grid(
-            row=6, column=1, padx=10, pady=5, sticky="w")
+            row=5, column=1, padx=10, pady=5, sticky="w")
 
         run_button = ttk.Button(
             self.root, text="Run", command=self.start_process)
-        run_button.grid(row=7, columnspan=2, pady=10)
+        run_button.grid(row=6, columnspan=2, pady=10)
 
         exit_button = ttk.Button(
             self.root, text="Exit", command=self.root.destroy)
-        exit_button.grid(row=8, columnspan=2, pady=10)
+        exit_button.grid(row=7, columnspan=2, pady=10)
 
     def start_process(self):
         selected_device = self.device_var.get()
@@ -86,7 +80,6 @@ class GUIApp:
             selected_analyzer = True
         else:
             selected_analyzer = False
-        selected_exo = self.exo_var.get()
         selected_side = self.side_var.get()
         selected_muscles = int(self.muscles_var.get())
         channels_to_remove = self.remove_channels_var.get()
@@ -94,7 +87,6 @@ class GUIApp:
         rospy.set_param("/device", selected_device)
         rospy.set_param("/method", selected_method)
         rospy.set_param("/latency_analyzer", selected_analyzer)
-        rospy.set_param("/exo_connected", selected_exo)
         rospy.set_param("/side", selected_side)
         rospy.set_param("/muscle_count", selected_muscles)
         rospy.set_param("/channels_to_remove", channels_to_remove)
@@ -102,12 +94,13 @@ class GUIApp:
         print("Device:", selected_device)
         print("Method:", selected_method)
         print("Analyzer:", selected_analyzer)
-        print("Exo Connection:", selected_exo)
-        print("Side:", selected_side)
         print("Muscles:", selected_muscles)
         print("Removed Channels:", channels_to_remove)
 
         rospy.set_param("/startup_gui_completed", True)
+
+    def shutdown(self):
+        self.root.destroy()
 
 
 if __name__ == "__main__":
