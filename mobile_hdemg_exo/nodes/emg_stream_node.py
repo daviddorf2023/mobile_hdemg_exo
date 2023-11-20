dbcaeb34d111fc1e48d1572f5361e01ba2404ff1
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2023-11-19 21:51:23 -0600

    Cleanup and documentation

--- mobile_hdemg_exo/nodes/emg_stream_node.py
+++ mobile_hdemg_exo/nodes/emg_stream_node.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import rospy
-import numpy as np
 from std_msgs.msg import Float64MultiArray
 from mobile_hdemg_exo.msg import StampedFloat64MultiArray
 
@@ -22,11 +21,13 @@ class EMGStreamNode:
     A class for streaming and processing EMG data.
 
     Attributes:
-        streamer: An object that streams EMG data from the selected device.
-        processor: An object that processes EMG data using the selected method.
-        raw_pub: A ROS publisher for publishing raw EMG data.
-        processed_pub: A ROS publisher for publishing processed EMG data.
-        r: A ROS rate object for controlling the publishing rate.
+        r: A ROS Rate object.
+        start_time: The time at which the node was started.
+        emg_pub: A ROS publisher for the /hdEMG_stream_raw topic.
+        p: A GPIO PWM object.
+        streamer: An EMG streamer object.
+        emg_offset: The offset of the EMG data in the raw stream.
+        simulation_reading: A list of floats representing EMG data.
     """
 
     def __init__(self):
@@ -41,18 +42,13 @@ class EMGStreamNode:
         self.emg_pub = rospy.Publisher(
             'hdEMG_stream_raw', StampedFloat64MultiArray, queue_size=10)
 
-        # Initialize the PWM output pin
         if LATENCY_ANALYZER_MODE:
             import RPi.GPIO as GPIO
             GPIO.setmode(GPIO.BOARD)
             GPIO.setup(PWM_OUTPUT_PIN, GPIO.OUT, initial=GPIO.HIGH)
             self.p = GPIO.PWM(PWM_OUTPUT_PIN, 50)
             self.p.start(50)
-        else:
-            self.p = None
 
-        # Initialize the EMG streamer
-        self.emg_offset = 0
         if EMG_DEVICE == 'Quattrocento':
             from mobile_hdemg_exo.streamer.emg_qc_streamer import EMGQCStreamer
             self.streamer = EMGQCStreamer(MUSCLE_COUNT)
@@ -60,6 +56,7 @@ class EMGStreamNode:
         elif EMG_DEVICE == 'Muovi+Pro':
             from mobile_hdemg_exo.streamer.emg_muovi_streamer import EMGMUOVIStreamer
             self.streamer = EMGMUOVIStreamer(MUSCLE_COUNT)
+            self.emg_offset = 0
         elif EMG_DEVICE == 'File':
             from mobile_hdemg_exo.streamer.emg_file_streamer import EMGFileStreamer
             self.path = rospy.get_param(
@@ -69,21 +66,6 @@ class EMGStreamNode:
             self.emg_offset = 128
         else:
             raise ValueError('Invalid EMG_DEVICE')
-
-        self.simulation_reading = np.zeros(64 * MUSCLE_COUNT)
-
-    def simulation_callback(self, data):
-        """
-        Callback function for the /hdEMG_Simulation topic.
-
-        Args:
-            data: A list of floats representing EMG data.
-        """
-        self.simulation_reading = data.data
-
-    def pwm_cleanup(self):
-        self.p.stop()
-        GPIO.cleanup()
 
     def run_emg(self):
         """
@@ -105,4 +87,5 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         emg_stream_node.run_emg()
     if LATENCY_ANALYZER_MODE:
-        emg_stream_node.pwm_cleanup()
+        emg_stream_node.p.stop()
+        GPIO.cleanup()
